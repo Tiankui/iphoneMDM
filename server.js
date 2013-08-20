8c5229f3578353ddc74936ff1609346211b4cccb
Tiankui <eric.prototype@gmail.com>
2013-08-20 17:08:15 +0800

    add

--- server.js
+++ server.js
@@ -1,24 +1,49 @@
-var fs = require('fs');
-var express = require('express');
-var https = require('https');
-var app = express();
+var fs = require('fs'),
+    express = require('express'),
+    https = require('https'),
+    app = express(),
+    parser = require('xml2json'),
+    options = {
+      key: fs.readFileSync(__dirname+'/Certificates/server.key'),
+      cert: fs.readFileSync(__dirname+'/Certificates/server.pem')
+      //ca: [fs.readFileSync(__dirname+'/Certificates/ca.crt')]
+      //pfx: fs.readFileSync(__dirname+'/Certificates/tomcat.p12')
+    };
 
-var options = {
-  key: fs.readFileSync(__dirname+'/Certificates/server.key'),
-  cert: fs.readFileSync(__dirname+'/Certificates/server.pem'),
-  ca: [fs.readFileSync(__dirname+'/Certificates/ca.crt')]
-  //pfx: fs.readFileSync(__dirname+'/Certificates/tomcat.p12')
-};
+app.use(express.bodyParser());
+app.use(express.logger('dev'));
 app.get('/',function (req,res) {
-  console.log(req);
   res.send('/');
 });
-app.all('/checkin',function (req,res) {
-  console.log(req);
-  res.send('checkin');
+
+app.put('/checkin',function (req,res) {
+  var content = '',
+      device = {};
+  req.on("data",function(data){
+    content += data;
+    var json_content = JSON.parse(parser.toJson(content)).plist.dict;
+    if (json_content.string[0] !== 'Authenticate') {
+      console.log('====================TokenUpdate=====================');
+      //取得信息
+      device = {
+        push_magic: json_content.string[1],
+        token: json_content.data,
+        topic: json_content.string[2],
+        uuid: json_content.string[3]
+      };
+      console.log(json_content);
+      console.log(device);
+      console.log('====================END=====================');
+    }else{
+      console.log("===============认证阶段可再次加判断中断================");
+      console.log(content);
+      console.log('\n\n\n\n');
+    }
+  });
+  res.send('//');
 });
+
 app.all('login',function (req,res) {
-  console.log(req);
   res.send('heino');
 });
 https.createServer(options,app).listen(8000);
