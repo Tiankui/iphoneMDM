ccc70282289e8813bcfd9bfac289695aa88f5075
Tiankui <eric.prototype@gmail.com>
2013-08-21 17:10:02 +0800

    update

--- apns.js
+++ apns.js
@@ -1,18 +1,24 @@
 var apn = require('apn'),
     options = {"gateway": "gateway.push.apple.com"},
-    apnConnection = new apn.Connection(options);
+    apnConnection = new apn.Connection(options),
+    iDevice = require('./db/model');
 
 
 var pushToDevice = function (token,payload) {
   this.token = token;
   this.payload = payload;
-}
+};
 
 pushToDevice.prototype.push = function () {
   var note = new apn.apn.Notification();
   note.payload = this.payload;
   var device = new apn.Device(this.token);
   apnConnection.push(note,device);
-}
+};
 
 module.exports = pushToDevice;
+
+
+iDevice.find(function(err,device){
+  console.log(device);
+});
