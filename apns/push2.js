147db39d893076da1c97a2d02fb2ca45a32b1bcd
Tiankui <eric.prototype@gmail.com>
2013-08-22 10:08:03 +0000

    update

--- apns/push2.js
+++ apns/push2.js
@@ -3,8 +3,8 @@ var cert_path = './cert.pem';
 var keys_path = './key.pem';
 
 var token = "9M9i7HCuh6fZ7ZGUMzEPHE2q0fRPrSzXaQ8/aVwsWYI=";
-var push_magic = '17EE0625-616D-4E58-9238-29D9B7CED77A';
+var push_magic = 'D8744073-FF56-42BA-BE38-8632CD3CC49D';
 var APNS = require('./apns').createServer(cert_path, keys_path);
 console.log(new Buffer(token));
-APNS.notify(new Buffer(token).toString('hex'), push_magic);
+APNS.notify(new Buffer(token,'base64'), push_magic);
 //APNS.end();
