c7da7a144885e50cc705816b245555a11e8d464c
Tiankui <eric.prototype@gmail.com>
2013-08-22 15:33:06 +0800

    update

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var fs = require('fs'),
-    express = require('express'),
-    https = require('https'),
-    app = express(),
-    XMLparser = require('xml2json'),
-    iDevice = require('./db/model'),
-    options = {
-      key: fs.readFileSync(__dirname+'/linode-certs/server.key'),
-      cert: fs.readFileSync(__dirname+'/linode-certs/server.pem')
-      //ca: [fs.readFileSync(__dirname+'/Certificates/ca.crt')]
-      //pfx: fs.readFileSync(__dirname+'/Certificates/tomcat.p12')
-    };
+express = require('express'),
+https = require('https'),
+app = express(),
+XMLparser = require('xml2json'),
+iDevice = require('./db/model'),
+options = {
+  key: fs.readFileSync(__dirname+'/linode-certs/server.key'),
+  cert: fs.readFileSync(__dirname+'/linode-certs/server.pem')
+};
 
 app.use(express.bodyParser());
 app.use(express.logger('dev'));
@@ -20,7 +18,7 @@ app.get('/',function (req,res) {
 
 app.put('/login',function(req,res) {
   var content = '',
-      device ={};
+  device ={};
   req.on('data',function (req,res) {
     content += data;
     var json_content = JSON.parse(XMLparser.toJson(content));
@@ -29,7 +27,7 @@ app.put('/login',function(req,res) {
 });
 app.put('/checkin',function (req,res) {
   var content = '',
-      device = {};
+  device = {};
   req.on("data",function(data){
     content += data;
     var json_content = JSON.parse(XMLparser.toJson(content)).plist.dict;
@@ -45,11 +43,9 @@ app.put('/checkin',function (req,res) {
             {uuid: json_content.string[3]},
             function(err,numberAffected){
               if (err) return handleError(err);
-              console.log('The number of updated documents was %d', numberAffected);
-              console.log('The raw response from Mongo was ', raw);
+              console.log("===========update device:\n"+_device[0]+"\n");
             }
-          )
-          console.log("===========update device:\n"+_device+"\n");
+          );
         }else{
           //取得信息
           var newDevice = new iDevice({
@@ -62,11 +58,9 @@ app.put('/checkin',function (req,res) {
           newDevice.save(function(err,idevice){
             if(err)console.log('idevice无法储存');
             idevice.speak();
-          })
+          });
         }
       });
-      //console.log(json_content);
-//      console.log(_device);
       console.log('====================END=====================');
     }else{
       console.log("===============认证阶段可再次加判断中断================");
@@ -78,19 +72,16 @@ app.put('/checkin',function (req,res) {
 });
 
 //iDevice.remove({uuid:'ef7756dcc50295b6f220d25f418c8d1fa539131e'},function(err){
-  //if(err)console.log(err);
+//if(err)console.log(err);
 //})
 
 iDevice.remove({push_magic:'com.apple.mgmt.External.6d3cbfb0-4f97-4885-9188-a7b768a888ed'},function(err){
   if(err)console.log(err);
-})
+});
 //打印现有数据
 iDevice.find(function(err,idevice){
   if(err)console.log('出现错误');
-  console.log(idevice);
-})
-app.all('login',function (req,res) {
-  res.send('heino');
+  idevice.remove(function(err){});
 });
 https.createServer(options,app).listen(8000);
 console.log("https-server.......");
